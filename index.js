d3e75543244cb51f6bcab9f9b704a38e04a8d359
jkavanagh00 <jkavanagh00@gmail.com>
2026-02-01 19:56:36 +0100

    added functions to create and shuffle the array of cards, populate the html grid, handle the basic UI logic for player inputs, track matches and misses, and reset unmatched cards

--- index.js
+++ index.js
@@ -2,73 +2,199 @@ let cards = [
     {
         name: "battery",
         id: 1,
-        location: "images/battery-minus.png"
+        location: "images/battery-minus.png",
+        solved: false,
+        flipped: false
     },
-        {
+    {
         name: "biplane",
         id: 2,
-        location: "images/biplane.png"
+        location: "images/biplane.png",
+        solved: false,
+        flipped: false
     },
-        {
+    {
         name: "card-pickup",
         id: 3,
-        location: "images/card-pickup.png"
+        location: "images/card-pickup.png",
+        solved: false,
+        flipped: false
     },
-        {
+    {
         name: "card-play",
         id: 4,
-        location: "images/card-play.png"
+        location: "images/card-play.png",
+        solved: false,
+        flipped: false
     },
-        {
+    {
         name: "chocolate-bar",
         id: 5,
-        location: "images/chocolate-bar.png"
+        location: "images/chocolate-bar.png",
+        solved: false,
+        flipped: false
     },
-        {
+    {
         name: "hearts",
         id: 6,
-        location: "images/hearts.png"
+        location: "images/hearts.png",
+        solved: false,
+        flipped: false
     },
-        {
+    {
         name: "poppy",
         id: 7,
-        location: "./images/poppy.png"
+        location: "./images/poppy.png",
+        solved: false,
+        flipped: false
     },
-        {
+    {
         name: "thrust",
         id: 8,
-        location: "images/thrust.png"
+        location: "images/thrust.png",
+        solved: false,
+        flipped: false
     },
 ];
 
+let cardsShowing = false;
+let lastCard = null;
+let mistakes = 0;
+let matches = 0;
+
+
+// Fisher-Yates sorting algorithm;
 function shuffle(array) {
+    // iterate through the array backwards;
     for (let i = array.length - 1; i > 0; i--) {
+        // generate a random index that is lower than i;
         const j = Math.floor(Math.random() * (i + 1));
+        // swap the elements at i & j;
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
 
+// creates a doubled and shuffled copy of the hard-coded cards array above;
 function initialiseCardArray(cards) {
-    const newArray = cards.concat(cards);
+    // double card array so that each card appears twice;
+    cardsCopy = cards.map(card => ({ ...card }));
+    const newArray = cards.concat(cardsCopy);
+    // shuffle and return;
     const cardArray = shuffle(newArray);
     return cardArray;
 }
 
+// uses DOM manipulation to populate card grid html element with flippable card divs;
 function populateCardGrid(cardArray) {
+    // select game grid DOM element;
     const cardGrid = document.getElementById('game-grid');
+    // loop through game grid DOM element and add HTML using the innerHTML property;
     for (let i = 0; i < cardArray.length; i++) {
         const markup = `
         <div class="flip-card">
             <div class="flip-card-inner">
                 <div class="flip-card-front">
-                    <img src="back.png" alt="card back" style="width:100px;height:100px;border-radius: 40px;">
+                    <img src="images/back.png" alt="card back">
                 </div>
                 <div class="flip-card-back">
-                    <img src=${cardArray[i].location} alt="card front" style="width:300px;height:300px;border-radius: 40px;">
+                    <img src="${cardArray[i].location}" alt="card front">
                 </div>
             </div>
         </div>`;
         cardGrid.innerHTML += markup;
+    }
+
+    // add card data from array to DOM elements inside the cardData property
+    const domCards = document.querySelectorAll('.flip-card');
+    for (let i = 0; i < domCards.length; i++) {
+        domCards[i].cardData = cardArray[i];
+    }
+}
+
+function attachCardListeners() {
+    const cards = document.querySelectorAll('.flip-card');
+
+    // iterate through array of DOM cards;
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+
+        // add event listeners to DOM cards to control UI input logic;
+        card.addEventListener('click', function () {
+
+            // check if card is already face up and do nothing if it is;
+            if (card.cardData.flipped === true) {
+                console.log("revealed card clicked")
+
+                // if card is face down go to main UI control flow;
+            } else {
+                // if there are no cards showing;
+                if (!cardsShowing) {
+                    // update boolean to track that a card is revealed;
+                    cardsShowing = true;
+                    // update number to store the index of this card;
+                    lastCard = i;
+                    // update flipped state on the DOM card;
+                    card.cardData.flipped = true;
+                } else {
+                    // check if the current card and the last card have the same id;
+                    if (card.cardData.id === cards[lastCard].cardData.id) {
+                        // update the solved state on both DOM card;
+                        card.cardData.solved = true;
+                        cards[lastCard].cardData.solved = true;
+                        matches++;
+                        console.log(`matches: ${matches}`);
+                    } else {
+                        mistakes++;
+                        console.log(`mistakes: ${mistakes}`)
+                    }
+                    // update boolean to track that no card is revealed;
+                    cardsShowing = false;
+                    // call function to reset flipped cards;
+                    resetCards();
+                    // reset last card to null state;
+                    lastCard = null;
+                    // update boolean to track that card is face up;
+                    card.cardData.flipped = true;
+                }
+                // reveal the card by toggling the flipped CSS class;
+                this.classList.toggle('flipped');
+            }
+        });
+    }
+}
+
+function resetCards() {
+    const cards = document.querySelectorAll('.flip-card');
+
+    // set a 1.5 second timer before calling the main function logic
+    setTimeout(() => {
+        // iterate through all DOM cards
+        for (let i = 0; i < cards.length; i++) {
+            const card = cards[i];
+            // check that card is face up and that it has not been solved
+            if (card.cardData.flipped === true && card.cardData.solved === false) {
+                // flip the card face down
+                card.classList.toggle('flipped');
+                // update boolean to track that card is face down
+                card.cardData.flipped = false;
+            }
+        }
+        // time out length in milliseconds
+    }, 1500);
+}
+
+function resetAllCards() {
+    const cards = document.querySelectorAll('.flip-card');
+    // iterate through all DOM cards
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        // check that the card is face up
+        if (card.cardData.flipped === true) {
+            // flip the card face down
+            card.classList.toggle('flipped');
+            // update boolean to track that card is face down
+            card.cardData.flipped = false;
+        }
     }
 }
