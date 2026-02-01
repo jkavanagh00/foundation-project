b9c109196a9f2ea06c0d4a958345c4eb4ec7235a
hiwot <99hiwot@gmail.com>
2026-02-01 15:13:55 +0100

    spelling checkup

--- index.js
+++ index.js
@@ -1,64 +1,66 @@
+/** @format */
+
 let cards = [
-    {
-        name: "battery",
-        id: 1,
-        location: "images/battery-minus.png"
-    },
-        {
-        name: "biplane",
-        id: 2,
-        location: "images/biplane.png"
-    },
-        {
-        name: "card-pickup",
-        id: 3,
-        location: "images/card-pickup.png"
-    },
-        {
-        name: "card-play",
-        id: 4,
-        location: "images/card-play.png"
-    },
-        {
-        name: "chocolate-bar",
-        id: 5,
-        location: "images/chocolate-bar.png"
-    },
-        {
-        name: "hearts",
-        id: 6,
-        location: "images/hearts.png"
-    },
-        {
-        name: "poppy",
-        id: 7,
-        location: "images/poppy.png"
-    },
-        {
-        name: "thrust",
-        id: 8,
-        location: "images/thrust.png"
-    },
+  {
+    name: "battery",
+    id: 1,
+    location: "images/battery-minus.png",
+  },
+  {
+    name: "biplane",
+    id: 2,
+    location: "images/biplane.png",
+  },
+  {
+    name: "card-pickup",
+    id: 3,
+    location: "images/card-pickup.png",
+  },
+  {
+    name: "card-play",
+    id: 4,
+    location: "images/card-play.png",
+  },
+  {
+    name: "chocolate-bar",
+    id: 5,
+    location: "images/chocolate-bar.png",
+  },
+  {
+    name: "hearts",
+    id: 6,
+    location: "images/hearts.png",
+  },
+  {
+    name: "poppy",
+    id: 7,
+    location: "images/poppy.png",
+  },
+  {
+    name: "thrust",
+    id: 8,
+    location: "images/thrust.png",
+  },
 ];
 
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
-function initialiseCardArray(cards) {
-    const newArray = cards.concat(cards);
-    const cardArray = shuffle(newArray);
-    return cardArray;
+function initializeCardArray(cards) {
+  const newArray = cards.concat(cards);
+  const cardArray = shuffle(newArray);
+  return cardArray;
 }
 
 function populateCardGrid(cardArray) {
-    const cardGrid = document.getElementById('game-grid');
-    for (let i = 0; i < cardArray.length; i++) {
-        const markup = `
+  const cardGrid = document.getElementById("game-grid");
+  for (let i = 0; i < cardArray.length; i++) {
+    const markup = `
         <div class="flip-card">
             <div class="flip-card-inner">
                 <div class="flip-card-front">
@@ -68,8 +70,8 @@ function populateCardGrid(cardArray) {
                     <img src=${cardArray[i].location} alt="card front" style="width:300px;height:300px;border-radius: 40px;">
                 </div>
             </div>
-        </div>`
-    }
+        </div>`;
+  }
 }
 
-console.log(initialiseCardArray(cards))
+console.log(initialiseCardArray(cards));
