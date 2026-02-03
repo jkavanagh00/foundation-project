5f9b01f4d552c11e5a25c806cdd854205c2eb8bb
jkavanagh00 <jkavanagh00@gmail.com>
2026-02-03 13:09:16 +0100

    added a variable to expose game globally and fix bug in new game button

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import Game from "./game.js"
 
 const game = new Game(8);
 game.populateCardGrid(game.initialiseCardArray(game.cards));
+
+// makes game class accessible to the DOM so that onclick handlers can access it;
+window.game = game;
