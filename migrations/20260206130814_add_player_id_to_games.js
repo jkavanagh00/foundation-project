50105c4612a5d2053e5963cfaa7f6cda94300ece
hiwot <99hiwot@gmail.com>
2026-02-07 20:40:42 +0100

    player id added to games table

---
+++ migrations/20260206130814_add_player_id_to_games.js
@@ -0,0 +1,22 @@
+/**
+ * @format
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = function (knex) {
+  return knex.schema.table("games", function (table) {
+    table
+      .integer("player_id")
+      .unsigned()
+      .references("id")
+      .inTable("players")
+      .onDelete("CASCADE");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
