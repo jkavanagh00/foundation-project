95b66d3c8f26aa299ba80d408bd68742821e916c
hiwot <99hiwot@gmail.com>
2026-02-07 20:35:43 +0100

    players table

---
+++ migrations/20260206122555_create_players_table.js
@@ -0,0 +1,21 @@
+/**
+ * @format
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = function (knex) {
+  return knex.schema.createTable("players", function (table) {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("players");
+};
