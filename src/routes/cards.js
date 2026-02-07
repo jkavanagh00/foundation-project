d1a87eb4a930fc2b1f57a0257d1fa2e162b77f5e
hiwot <99hiwot@gmail.com>
2026-02-07 20:56:21 +0100

    cards API

---
+++ src/routes/cards.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+//GET cards for a deck
+router.get("/deck/:deckId", async (req, res) => {
+  const cards = await db("cards").where({
+    deck_id: requestAnimationFrame.params.deckId,
+  });
+  res.json(cards);
+});
+//CREATE a card
+router.post("/", async (req, res) => {
+  const { deck_id, front, back } = req.body;
+  const [id] = await db("cards").insert({ deck_id, front, back });
+  res.json({ id, deck_id, front, back });
+});
+module.exports = router;
