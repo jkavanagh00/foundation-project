13745e0169e06e658f3998bfef557dcb07a32e62
hiwot <99hiwot@gmail.com>
2026-02-07 20:51:39 +0100

    knex file

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+/** @format */
+
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: "localhost",
+      user: "root",
+      password: " ",
+      database: "flipmaster_game",
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+  },
+};
