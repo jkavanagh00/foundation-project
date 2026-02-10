76a5812b0467fff67d71f0612bb84fb8c5963963
jkavanagh00 <jkavanagh00@gmail.com>
2026-02-10 13:19:45 +0100

    added path to app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const leaderboardRouter = require("./routes/leaderboard");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..")));
 
 //routes would go here
 app.use("/decks", require("./routes/decks"));
