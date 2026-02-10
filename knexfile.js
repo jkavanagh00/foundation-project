0259ae17097d91b5e5274b1317e9c54ef3b443ee
jkavanagh00 <jkavanagh00@gmail.com>
2026-02-10 16:16:24 +0100

    first attempt to make this render friendly

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 /** @format */
 
+require('dotenv').config();
+
 // Update with your config settings.
 
 /**
@@ -9,13 +11,32 @@ module.exports = {
   development: {
     client: "mysql2",
     connection: {
-      host: "localhost",
-      user: "root",
-      password: "flipmaster",
-      database: "flipmaster_game",
+      host: process.env.DATABASE_HOST || "localhost",
+      user: process.env.DATABASE_USER || "root",
+      password: process.env.DATABASE_PASSWORD || "flipmaster",
+      database: process.env.DATABASE_NAME || "flipmaster_game",
     },
     migrations: {
       directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
     },
   },
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 /** @format */
 
 const app = require("./app");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
